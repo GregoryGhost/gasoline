f1c0624b47c83cc1f12bcdeddfd261b5b9aa2b6e
Gregory <lobster287@gmail.com>
2017-12-25 00:58:14 +0700

    Add prototype GUI of program

---
+++ GasoLine/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GasoLine
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var map = new List<List<int>>();
+            var l1 = new List<int> { 1, 2, 3, 4, 5 };
+            var l2 = new List<int> { 1, 2, 3, 4, 4 };
+            map.Add(l1);
+            map.Add(l2);
+            Map.ItemsSource = map;
+        }
+    }
+}
