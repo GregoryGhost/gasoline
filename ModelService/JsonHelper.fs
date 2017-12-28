4fd34d9faf1a149574aadec163e3d6e4619f1a13
Gregory <lobster287@gmail.com>
2017-12-28 21:34:41 +0700

    Add definition function for work with JSON

--- ModelService/JsonHelper.fs
+++ ModelService/JsonHelper.fs
@@ -1,4 +1,5 @@
-﻿module JsonHelper
+﻿namespace ModelService
+module JsonHelper =
  open FSharp.Data
  open FSharp.Data.JsonExtensions
 
@@ -17,22 +18,7 @@
        "resistanceWithMedian" : 1,
        "tankCapacity": 10
        }]""">
-
- type Environment = 
-    | Air = 1
-    | Ground = 2
-    | Asphalt = 3
-    | Dirt = 4 //грязь
-    | Space = 5
-    | RuggedTerrain = 6//пересеченная местность
-
- type Vehicle = {
-                name : string;     //ограничение pattern от 1 до 20 символов - цифр или букв, включая - _
-         enginePower : int;        //ограничения pattern  от 1 до 1 000 000 000 л.с.
-              weight : double;     //ограничение pattern от 1 до 10 000 кг - double
-resistanceWithMedian : Environment;//ограничения pattern - воздух, вода, космос, земля, асфальт, грязь, пересеченная_местность
-        tankCapacity : int        }//ограничения pattern - от 0 до 10 000 л
-
+ 
  let s = "test.json"
 //TODO: сделать нормальную сериализацию списка Vehicle в JSON
  //let writeJSON v
@@ -50,6 +36,15 @@ resistanceWithMedian : Environment;//ограничения pattern - возду
          w
          sw.Close()
      write
+ 
+ type writeJson = JParameters list-> unit
+ type readJson = unit -> JParameters list
+
+ type toJson = Vehicle -> JParameters
+ type toJsons = Vehicle list -> JRecords
+
+ type toVehicles = JRecords -> Vehicle list
+ type toVehicle = JParameters -> Vehicle
 //Десерилизация данных из JSON файла
  let readJson =
      let testP = JRecords.Load(s)
