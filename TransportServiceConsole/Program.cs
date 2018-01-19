ad65675f8688af2779f92cbbef2aa2fa8a34c61f
Gregory <lobster287@gmail.com>
2018-01-19 19:26:28 +0700

    Added little sample using library F# from console project on C#

--- TransportServiceConsole/Program.cs
+++ TransportServiceConsole/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ModelService;
+using Library1;
 
 namespace TransportServiceConsole
 {
@@ -11,7 +12,24 @@ namespace TransportServiceConsole
     {
         static void Main(string[] args)
         {
+            var c = AutoShow.Instance;
+            var vehicle = new Vehicle("mazda", 1000, 200.5, ModelService.Environment.Asphalt, 900);
+            c.AddVehicle(vehicle);
+            c.AddVehicle(vehicle);
+            c.AddVehicle(vehicle);
+            var path = System.IO.Directory.GetCurrentDirectory() + "\\" + "test.json";
+            c.Save(path);
+            c.Load(path);
+            var t = c.GetAllVehicles;
+            WriteAllVehicle(t);
+        }
 
+        static void WriteAllVehicle(Vehicle[] vehicles)
+        {
+            foreach (var e in vehicles)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
