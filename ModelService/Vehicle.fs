35c4ff269085bfa4459bc7ecc0e48650c7ae888e
Gregory <lobster287@gmail.com>
2018-02-24 17:06:10 +0700

    Added a calculation of fuel consumption depending on the type of environment

--- ModelService/Vehicle.fs
+++ ModelService/Vehicle.fs
@@ -11,14 +11,38 @@ type Environment =
     | NANI = 0
 
 type Vehicle = {
-                name : string;     //ограничение pattern от 1 до 20 символов - цифр или букв, включая - _
-         enginePower : int;        //ограничения pattern  от 1 до 1 000 000 000 л.с.
-              weight : double;     //ограничение pattern от 1 до 10 000 кг - double
-resistanceWithMedian : Environment;//ограничения pattern - воздух, вода, космос, земля, асфальт, грязь, пересеченная_местность
-        tankCapacity : int        }//ограничения pattern - от 0 до 10 000 л
+                 name : string;     //ограничение pattern от 1 до 20 символов - цифр или букв, включая - _
+          enginePower : int;        //ограничения pattern  от 1 до 1 000 000 000 л.с.
+               weight : double;     //ограничение pattern от 1 до 10 000 кг - double
+ resistanceWithMedian : Environment;//ограничения pattern - воздух, вода, космос, земля, асфальт, грязь, пересеченная_местность
+         tankCapacity : int        }//ограничения pattern - от 0 до 10 000 л
 with
-    member internal this.CalcFuelConsumption = 
-           //resistanceWithMedian * (weight + tankCapacity) / enginePower
-           let calc = (this.weight + float this.tankCapacity) / float this.enginePower
+    member internal this.CalcFuelConsumption =
            let rs = this.resistanceWithMedian
-           calc * (float rs)
+           match rs with
+           | Environment.Air ->  //rs * (weight + tankCapacity) / enginePower
+               let calc = (this.weight + float this.tankCapacity) / float this.enginePower
+               calc * (float rs)
+           | Environment.Ground -> //rs - (weight + tankCapacity) / enginePower
+               let calc = (this.weight + float this.tankCapacity) / float this.enginePower
+               (float rs) - calc
+           | Environment.Asphalt -> //rs + (weight - tankCapacity) * enginePower
+               let calc = (this.weight + float this.tankCapacity) * float this.enginePower
+               (float rs) + calc
+           | Environment.Dirt ->
+                //resistanceWithMedian * weight + tankCapacity) / enginePower
+               let calc = (float rs) * this.weight 
+               calc + (float <| this.tankCapacity / this.enginePower)
+           | Environment.Space ->
+                //resistanceWithMedian * (weight + tankCapacity + enginePower)
+               let calc = this.weight 
+                          + float this.tankCapacity 
+                          + float this.enginePower
+               (float rs) * calc
+           | Environment.RuggedTerrain ->
+                //resistanceWithMedian * (weight / tankCapacity) - enginePower
+               let calc = (rs |> float)
+                          * (this.weight / (this.tankCapacity |> float))
+                          - (this.enginePower |> float)
+               calc
+           | _ -> 0.0
