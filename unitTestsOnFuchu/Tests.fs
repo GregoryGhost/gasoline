9ef6b929b9559f9c326752c359a72776b95cd447
Gregory <lobster287@gmail.com>
2018-03-01 14:30:20 +0700

    Moved the test cases to a separate file

--- unitTestsOnFuchu/Tests.fs
+++ unitTestsOnFuchu/Tests.fs
@@ -12,52 +12,6 @@ module Tests =
             TestListAddVehicle.testAddVehicle;
             CalcFuelConsumption.testValidVehicle;
             CalcFuelConsumption.testInvalidVehicle;
-            testList "Update vehicle to auto show" [
-                testCase "new record" <| (fun _ ->
-                    let newV = {validVehicle with enginePower = 225}
-
-                    autoShow.ClearAllVehicle()
-                    autoShow.AddVehicle(validVehicle) |> ignore
-                    let errors = autoShow.UpdateVehicle(validVehicle, newV)
-                    let expected = Seq.empty
-
-                    Assert.Equal("", expected, errors)
-                );
-
-                testCase "replaced the record of the same record" <| (fun _ ->
-                    autoShow.ClearAllVehicle()
-                    autoShow.AddVehicle(validVehicle) |> ignore
-
-                    let errors = autoShow.UpdateVehicle(validVehicle, validVehicle)
-                    let expected = Seq.empty
-
-                    Assert.Equal("", expected, errors)
-                );
-            ];
-            testList "Remove record from auto show" [
-                testCase "one record" <| (fun _ ->                   
-                    autoShow.ClearAllVehicle()
-                    autoShow.AddVehicle(validVehicle) |> ignore
-                    autoShow.RemoveVehicle(validVehicle)
-
-                    let result = autoShow.GetAllVehicles
-                    let expected = [] |> List.toSeq
-
-                    Assert.Equal("", expected, result)
-                );
-
-                testCase "some record" <| (fun _ ->
-                    let v1 = {validVehicle with enginePower = 222}
-
-                    autoShow.ClearAllVehicle()
-                    autoShow.AddVehicle(validVehicle) |> ignore
-                    autoShow.AddVehicle(v1) |> ignore
-                    autoShow.RemoveVehicle(validVehicle)
-
-                    let result = autoShow.GetAllVehicles
-                    let expected = [v1] |> List.toSeq
-
-                    Assert.Equal("", expected, result)
-                );
-            ];
+            TestAutoShow.testRemoveRecord;
+            TestAutoShow.testUpdateVehicle;
         ]
